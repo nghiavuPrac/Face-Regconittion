6eed33428dd278dbfdbd5575e088cede06f6bd62
nghiavuPrac <nghiavutrong1998@gmail.com>
2022-06-02 10:48:14 +0700

    commit again

---
+++ FacialRecognition/Dataset.py
@@ -0,0 +1,67 @@
+''''
+Capture multiple Faces from multiple users to be stored on a DataBase (dataset directory)
+	==> Faces will be stored on a directory: dataset/ (if does not exist, pls create one)
+	==> Each face will have a unique numeric integer ID as 1, 2, 3, etc                       
+
+Based on original code by Anirban Kar: https://github.com/thecodacus/Face-Recognition    
+
+Developed by Marcelo Rovai - MJRoBot.org @ 21Feb18    
+
+'''
+
+import cv2
+import os
+
+#path name for list of user
+pathName = "listOfUser.txt"
+
+cam = cv2.VideoCapture(0)
+cam.set(3, 1080) # set video width
+cam.set(4, 720) # set video height
+
+face_detector = cv2.CascadeClassifier('E:\Project Face regconition\OpenCV-Face-Recognition\FacialRecognition\haarcascade_frontalface_default.xml')
+
+# For each person, enter one numeric face id
+def loadId_from_file(pathName):
+    with open(pathName,'r') as file:
+        for line in file:
+            pass
+        face_id= line.split(",")[0]
+    return face_id
+face_id = loadId_from_file(pathName)
+
+
+print("\n [INFO] Initializing face capture. Look the camera and wait ...")
+# Initialize individual sampling face count
+count = 0
+
+while(True):
+
+    ret, img = cam.read()
+    img = cv2.flip(img, 1) # flip video image vertically
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_detector.detectMultiScale(gray, 1.3, 5)
+
+    for (x,y,w,h) in faces:
+
+        cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)     
+        count += 1
+
+        # Save the captured image into the datasets folder
+        cv2.imwrite("E:\Project Face regconition\OpenCV-Face-Recognition\FacialRecognition\dataset/User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
+
+        cv2.imshow('image', img)
+
+    k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
+    print(count)
+    if k == 27:
+        break
+    elif count >= 30: # Take 30 face sample and stop video
+        break
+
+# Do a bit of cleanup
+print("\n [INFO] Exiting Program and cleanup stuff")
+cam.release()
+cv2.destroyAllWindows()
+
+
---
+++ FacialRecognition/Recognition.py
@@ -0,0 +1,84 @@
+''''
+Real Time Face Recogition
+	==> Each face stored on dataset/ dir, should have a unique numeric integer ID as 1, 2, 3, etc                       
+	==> LBPH computed model (trained faces) should be on trainer/ dir
+Based on original code by Anirban Kar: https://github.com/thecodacus/Face-Recognition    
+
+Developed by Marcelo Rovai - MJRoBot.org @ 21Feb18  
+
+'''
+
+import cv2
+import numpy as np
+import os 
+
+pathName = "listOfUser.txt"
+
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read('E:\Project Face regconition\OpenCV-Face-Recognition\FacialRecognition\Trainer\Trainer.yml')
+cascadePath = "E:\Project Face regconition\OpenCV-Face-Recognition\FacialRecognition\haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascadePath)
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+id = 0
+
+def load_from_file(pathName):
+    users = dict()
+    with open(pathName,'r') as file:
+        for line in file:
+            user = line.rstrip().split(",")
+            users[user[0]] = user[1]
+    return users
+names= load_from_file(pathName)
+
+# Initialize and start realtime video capture
+cam = cv2.VideoCapture(0)
+cam.set(3, 1080) # set video widht
+cam.set(4, 720) # set video height
+
+# Define min window size to be recognized as a face
+minW = 0.1*cam.get(3)
+minH = 0.1*cam.get(4)
+
+while True:
+
+    ret, img =cam.read()
+    img = cv2.flip(img, 1) # Flip vertically
+
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale( 
+        gray,
+        scaleFactor = 1.2,
+        minNeighbors = 5,
+        minSize = (int(minW), int(minH)),
+       )
+
+    for(x,y,w,h) in faces:
+
+        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+
+        id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+
+        # Check if confidence is less them 65 ==> "0" is perfect match 
+        if (confidence < 65):
+            id = names[str(id)]
+            confidence = "  {0}%".format(round(100 - confidence))
+        else:
+            id = "unknown"
+            confidence = "  {0}%".format(round(100 - confidence))
+        
+        cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
+        cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
+    
+    cv2.imshow('camera',img) 
+
+    k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
+    if k == 27:
+        break
+
+# Do a bit of cleanup
+print("\n [INFO] Exiting Program and cleanup stuff")
+cam.release()
+cv2.destroyAllWindows()
